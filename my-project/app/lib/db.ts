6e22ef834f7c296ba8dba3b6a33ffe232e740a25
nguyenlongtrong2k4 <longtrongmusicmake@gmail.com>
2026-05-06 10:15:48 +0700

    Deploy from new folder

--- my-project/app/lib/db.ts
+++ my-project/app/lib/db.ts
@@ -1,10 +1,11 @@
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_nextjs",
+  host: process.env.MYSQL_HOST || "localhost",
+  user: process.env.MYSQL_USER || "root",
+  password: process.env.MYSQL_PASSWORD || "",
+  database: process.env.MYSQL_DATABASE || "db_nextjs",
+  port: Number(process.env.MYSQL_PORT) || 3306,
 });
 
 export default db;
